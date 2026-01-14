15c82f42a94b057431ecf92d35eb11306f35e4c8
tbirahim <gayethiernobirahim3@gmail.com>
2026-01-14 14:09:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ with tabs[0]:
     
     st.write("##")
     # Image d'un plat sénégalais (Thiéboudienne ou Yassa)
-    st.image("https://images.unsplash.com/photo-1627308595186-e6bb36712645?q=80&w=1974", caption="Notre célèbre Thiéboudienne Rouge", use_container_width=True)
+    st.image("https://www.afrocuisinemagazine.com/post/le-tchep-un-plat-mille-histoires-voici-3-choses-a-savoir-pour-le-reussir", caption="Notre célèbre Thiéboudienne Rouge", use_container_width=True)
     
     st.markdown("""
     <div style="text-align: center; padding: 20px;">
@@ -177,4 +177,5 @@ with tabs[4]:
             conn.cursor().execute("DELETE FROM commandes"); conn.commit(); st.rerun()
         df_adm = pd.read_sql('SELECT * FROM commandes ORDER BY id DESC', conn)
         st.dataframe(df_adm)
+
 
